cb6665b5d623e4f89b99fc1608738476f300e3c1
emagdnimMQT <mark.w.novak@gmail.com>
2021-06-09 11:36:15 -0400

    added home page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './pages/home-page'
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <HomePage />
       </div>
     )
   }
