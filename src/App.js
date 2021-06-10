6b222660a461dcbf0b39910f0e863febaeb86ab2
emagdnimMQT <mark.w.novak@gmail.com>
2021-06-10 13:43:28 -0400

    Installed react bootstrap and created Footer component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/home-page';
 import LocationsPage from './pages/locations-page';
 import ContactPage from './pages/contact-page';
+import Footer from './components/footer';
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
         <HomePage />
         <LocationsPage />
         <ContactPage />
+        <Footer />
       </div>
     )
   }
---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <Row>
+                <Col>
+                    <h1>Helloooooo</h1>
+                </Col>
+            </Row>
+        )
+    }
+}
+
+export default Footer;
