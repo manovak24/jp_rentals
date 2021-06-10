41d343428d94714ea33c4caef0170ac06c715087
emagdnimMQT <mark.w.novak@gmail.com>
2021-06-10 13:26:04 -0400

    Added location and contact page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './pages/home-page'
+import HomePage from './pages/home-page';
+import LocationsPage from './pages/locations-page';
+import ContactPage from './pages/contact-page';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <HomePage />
+        <LocationsPage />
+        <ContactPage />
       </div>
     )
   }
